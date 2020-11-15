42d893bf19d3a36dcc4fedf06ecd5cc012cc0b7f
talzaidman5 <49636399+talzaidman5@users.noreply.github.com>
2020-11-15 18:44:04 +0200

    Update GreatJob.java

--- app/src/main/java/com/example/a21a10243_project1/GreatJob.java
+++ app/src/main/java/com/example/a21a10243_project1/GreatJob.java
@@ -3,6 +3,7 @@ package com.example.a21a10243_project1;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
